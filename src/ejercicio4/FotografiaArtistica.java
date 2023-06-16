c29841094dc1418f8da94fb789659f997029f858
Eduardo Vibart <eduvibart@hotmail.com>
2023-06-16 14:39:00 -0300

    nombres mas visibles

--- src/ejercicio4/FotografiaArtistica.java
+++ src/ejercicio4/FotografiaArtistica.java
@@ -3,22 +3,22 @@ package ejercicio4;
 public class FotografiaArtistica {
 
 	public static void main(String[] args) {
-		char A[]= {'.', 'f', 'a', '.', 'e', 'a', '.', 'f'};
+		char A[]= {'.', 'f', 'a', 'e', 'e', 'a', '.', 'f'};
 		int X=1;
 		int Y=2;
 		int N=A.length;
 		System.out.println(fotografiaArtistica(A,N,X,Y));
 	}
 	
-	public static int fotografiaArtistica(char A[], int N, int X, int Y) {
+	public static int fotografiaArtistica(char arreglo[], int tam, int min, int max) {
 		int resultado=0;
 		//recorro primero de izquierda a derecha
-		for (int i = 0; i+X < N; i++) {
-	        if (A[i] == 'f') {
-	            for (int j = i + X; j <= i + Y && j < N; j++) {
-	                if (A[j] == 'a') {
-	                    for (int k = j + X; k <= j + Y && k < N; k++) {
-	                        if (A[k] == 'e') {
+		for (int indiceFot = 0; indiceFot+min < tam; indiceFot++) {
+	        if (arreglo[indiceFot] == 'f') {
+	            for (int indiceArt = indiceFot + min; indiceArt <= indiceFot + max && indiceArt < tam; indiceArt++) {
+	                if (arreglo[indiceArt] == 'a') {
+	                    for (int indiceEscen = indiceArt + min; indiceEscen <= indiceArt + max && indiceEscen < tam; indiceEscen++) {
+	                        if (arreglo[indiceEscen] == 'e') {
 	                            resultado++;
 	                        }
 	                    }
@@ -27,17 +27,17 @@ public class FotografiaArtistica {
 	        }
 	    }
 		//recorro de derecha a izquierda
-		for (int i = N-1; i-X>0; i--) {
+		for (int indiceFot = tam-1; indiceFot-min>0; indiceFot--) {
 			
-			if (A[i]=='f') {
+			if (arreglo[indiceFot]=='f') {
 				
-				for (int j=i-X;j>=i-Y && j>=0; j--) {
+				for (int indiceArt=indiceFot-min;indiceArt>=indiceFot-max && indiceArt>=0; indiceArt--) {
 					
-	                if (A[j] == 'a') {
+	                if (arreglo[indiceArt] == 'a') {
 	                	
-	                	for (int k = j-X;k>=j-Y && k>=0;k--) {
+	                	for (int indiceEscen = indiceArt-min;indiceEscen>=indiceArt-max && indiceEscen>=0;indiceEscen--) {
 	                		
-	                		if (A[k] == 'e') {
+	                		if (arreglo[indiceEscen] == 'e') {
 	                            resultado++;
 	                        }
 	                	}
